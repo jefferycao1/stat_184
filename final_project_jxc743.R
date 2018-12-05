b3bdfca75fc6672b4d3861e2d9425c7e6c2467d1
Jeffery Cao <jefferycao2016@gmail.com>
2018-12-05 00:27:59 -0500

    Update final_project_jxc743.R

--- final_project_jxc743.R
+++ final_project_jxc743.R
@@ -22,14 +22,18 @@ datacleaning1 <- function(testdata){
 }
 
 # TODO
-# get unique number and then sum the minutes and divide it. Remove -1 users too. 
+# Remove -1 users
 datacleaningjobid <- function(testdata, jobidnumber){
   x <- testdata[which(testdata$jobid == jobidnumber)]
+  numunique <- length(unique(x$tucaseid))
   rows <- nrow(x)
-  print(cat("Number of data rows for", jobidnumber, "is:", rows))
-  x <- dcast(x, activity~., mean, na.rm = T, value.var="tuactdur24")
+  print(paste("Number of data rows for", jobidnumber, "is:", rows))
+  x <- dcast(x, activity~., sum, na.rm = T, value.var="tuactdur24")
   setnames(x, c("."), c("Total_Time"))
   x <- x[order(-x$Total_Time),]
+  
+  print("Calculating a new column variable, number of hours per person on average")
+  x$hoursperperson = x$Total_Time / (numunique * 60)
   return (x)
 }
 
@@ -71,6 +75,31 @@ graphtotaltime <- function(totaltime)
   plot(c)
 }
 
+# graphing total time spent on activities depending on job
+graphjob <- function(jobtimedata, jobstitle)
+{
+  
+  c <- ggplot(head(jobtimedata, 10),aes(x=activity,y=hoursperperson))+geom_col()+
+    ggtitle(paste("Total Time spent on Activities in Hours (Top 10) for", jobstitle))+
+    theme(plot.title = element_text(hjust = 0.5))+
+    xlab("Activity")+ylab("Time in hours across all respondents")
+  c <- c + scale_x_discrete(labels = c("Eating and drinking" = "Food", 
+                                       "Socializing and communicating with others" = "Socializing", 
+                                       "Television and movies (not religious)" = "TV",
+                                       "Washing, dressing and grooming oneself" = "Showering", 
+                                       "Food and drink preparation" = "Cooking", "Interior cleaning" = "Cleaning", 
+                                       "Reading for personal interest" = "Reading", 
+                                       "Relaxing, thinking" = "Relaxing", 
+                                       "Computer use for leisure (exc. Games)" = "Gaming", 
+                                       "Shopping, except groceries, food and gas" = "Shopping", 
+                                       "Travel related to working" = "Work-related Travel"
+                                       ))
+  #c + scale_x_discrete(labels = abbreviate)
+  c <- c + labs(caption = paste("INFO: The top 10 time spend on activites for", jobstitle,"Numbers may look weird due to weekends."))
+  print(paste("plotting second graph about", jobstitle))
+  plot(c)
+}
+
 
 
 main<-function(){
@@ -102,11 +131,27 @@ main<-function(){
   testdata = mergejob(testdata, jobinfocopy)
   
   print("--------------------------")
-  print("data preprocessing for next graph")
+  print("data preprocessing for next graph (job = software development)")
   softwaredevelopment = datacleaningjobid(testdata, 1020)
-  print("graph average time spent in software development")
+  print("graph average time spent for software developers")
+  graphjob(softwaredevelopment, "Software Developers")
+  print("")
+  print("ANALYSIS: Software developers have relatively the same top 10")
+  print("as everyone else but computer gaming enters the top 10")
   
-  return(softwaredevelopment)
+  print("-------------------------")
+  print("data preprocessing for next graph (job = lawyers)")
+  lawyers = datacleaningjobid(testdata, 2100)
+  print("graph average time spend for lawyers")
+  graphjob(lawyers, "Lawyers")
+  print("")
+  print("ANALYSIS: Graph averages for sleeping and work are again similiar to Software developers")
+  print("Computer Gaming is gone but travel related to work has shown up in the top 10")
+
+  # next do by industry graphs, make another variable while doing that and then do random forest
+  # then we are done. 
+  
+  return(testdata)
 }
 
 
