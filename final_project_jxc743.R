3b2f584adc2610443f3b0a362d90d6743b2c87db
Jeffery Cao <jefferycao2016@gmail.com>
2018-12-05 11:01:11 -0500

    Update final_project_jxc743.R

--- final_project_jxc743.R
+++ final_project_jxc743.R
@@ -21,8 +21,8 @@ datacleaning1 <- function(testdata){
   return(totaltime)
 }
 
-# TODO
-# Remove -1 users
+
+# Formats the data to get the most popular activities for each job id 
 datacleaningjobid <- function(testdata, jobidnumber){
   x <- testdata[which(testdata$jobid == jobidnumber)]
   numunique <- length(unique(x$tucaseid))
@@ -36,6 +36,23 @@ datacleaningjobid <- function(testdata, jobidnumber){
   x$hoursperperson = x$Total_Time / (numunique * 60)
   return (x)
 }
+
+# Formats the data to get the most popular activities for each industry
+datacleaningindustry<- function(testdata, industryidcode)
+{
+  x <- testdata[which(testdata$industryid == industryidcode)]
+  numunique <- length(unique(x$tucaseid))
+  rows <- nrow(x)
+  print(paste("Number of data rows for", industryidcode , "is:", rows))
+  x <- dcast(x, activity~., sum, na.rm = T, value.var="tuactdur24")
+  setnames(x, c("."), c("Total_Time"))
+  x <- x[order(-x$Total_Time),]
+  
+  print("Calculating a new column variable, number of hours per person on average")
+  x$hoursperperson = x$Total_Time / (numunique * 60)
+  return (x)
+}
+
 
 # merge the occupations to the data dataframe
 mergeoccupation<-function(datacopy, codescopy){
@@ -100,7 +117,28 @@ graphjob <- function(jobtimedata, jobstitle)
   plot(c)
 }
 
-
+graphindustry <- function(industrydata, industryname)
+{
+  c <- ggplot(head(industrydata, 10),aes(x=activity,y=hoursperperson))+geom_col()+
+    ggtitle(paste("Total Time spent on Activities in Hours (Top 10) in industry", industryname))+
+    theme(plot.title = element_text(hjust = 0.5))+
+    xlab("Activity")+ylab("Time in hours across all respondents")
+  c <- c + scale_x_discrete(labels = c("Eating and drinking" = "Food", 
+                                       "Socializing and communicating with others" = "Socializing", 
+                                       "Television and movies (not religious)" = "TV",
+                                       "Washing, dressing and grooming oneself" = "Showering", 
+                                       "Food and drink preparation" = "Cooking", "Interior cleaning" = "Cleaning", 
+                                       "Reading for personal interest" = "Reading", 
+                                       "Relaxing, thinking" = "Relaxing", 
+                                       "Computer use for leisure (exc. Games)" = "Gaming", 
+                                       "Shopping, except groceries, food and gas" = "Shopping", 
+                                       "Travel related to working" = "Work-related Travel"
+  ))
+  #c + scale_x_discrete(labels = abbreviate)
+  c <- c + labs(caption = paste("INFO: The top 10 time spend on activites for", industryname,"Numbers may look weird due to weekends."))
+  print(paste("plotting second graph about", industryname))
+  plot(c)
+}
 
 main<-function(){
   # data from kaggle https://www.kaggle.com/bls/american-time-use-survey
@@ -135,23 +173,52 @@ main<-function(){
   softwaredevelopment = datacleaningjobid(testdata, 1020)
   print("graph average time spent for software developers")
   graphjob(softwaredevelopment, "Software Developers")
-  print("")
-  print("ANALYSIS: Software developers have relatively the same top 10")
+  print("ANALYSIS:")
+  print("Software developers have relatively the same top 10")
   print("as everyone else but computer gaming enters the top 10")
   
   print("-------------------------")
   print("data preprocessing for next graph (job = lawyers)")
   lawyers = datacleaningjobid(testdata, 2100)
-  print("graph average time spend for lawyers")
+  print("graph average time spent for lawyers")
   graphjob(lawyers, "Lawyers")
-  print("")
-  print("ANALYSIS: Graph averages for sleeping and work are again similiar to Software developers")
+  print("ANALYSIS")
+  print("Graph averages for sleeping and work are again similiar to Software developers")
   print("Computer Gaming is gone but travel related to work has shown up in the top 10")
 
   # next do by industry graphs, make another variable while doing that and then do random forest
   # then we are done. 
   
-  return(testdata)
+
+  print("-------------------------")
+  print("data preprocessing for next graph (industry = Air Transportation")
+  air = datacleaningindustry(testdata, 6070)
+  print("graph average time spent for people in air transportation")
+  graphindustry(air, "Air Transportation")
+  print("Analysis")
+  print("Nothing new here, Everything looks comparable to the averages for Software developers")
+  print("and Lawyers, We will check it with another industry maybe to see changes.")
+  
+  print("-------------------------")
+  print("data preprocessing for next graph (industry = Banking")
+  banking = datacleaningindustry(testdata, 6870)
+  print("graph average time spent for people in banking")
+  graphindustry(banking, "Banking")
+  print("Analysis")
+  print("Surprising that Air Transportation people sleep more on average than Bankers")
+  print("But the top 10 activities done looks the same for bankers and Air transportation")
+  print("FINAL ANALYSIS")
+  print("People from all industries and jobs seem to spend roughly the same amount of time on")
+  print("their top 10 activities. Sleeping is the most and then Work. The only deviation was that")
+  print("Software developers spend mroe time playing video games/computer games")
+  
+  
+  print("-------------------------")
+  print("-------------------------")
+  print("FINAL PART")
+  
+  
+  return(air)
 }
 
 
